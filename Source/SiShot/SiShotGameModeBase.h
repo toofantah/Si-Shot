315f52c107710b328608df1b62de4c440f155dab
Toofan Tahmouresi <toofan.tah@gmail.com>
2023-02-19 17:32:34 +0300

    Update WorkSpace

---
+++ Source/SiShot/SiShotGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SiShotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SISHOT_API ASiShotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
